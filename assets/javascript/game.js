678e7bbc735f2b9d3bf142c53d3cea38a82313a5
jeffrey <jorea911@gmail.com>
2018-11-11 17:06:06 -0800

    final draft

---
+++ assets/javascript/game.js
@@ -0,0 +1,66 @@
+$(document).ready(function(){
+
+    var alphabet = [ "a", "b", "c", "d", "e","f","h","i","j","k","l","m","n", "o","p","q","r","s", "t","u","v","w","x","y","z"];
+    var randomLetter = "";
+    var lossesCounter = 0;
+    var guessesLeftCounter = 9;
+    var winsCounter = 0;
+
+
+    function getAlphabet(){
+        randomLetter = alphabet[Math.floor(Math.random()* alphabet.length)];
+        console.log("random letter generated:" + randomLetter);
+        guessesLeftCounter = 9;
+        $(".guessesSoFar").empty();
+        $(".guessesLeft").empty();
+        $(".guessesLeft").append(" " +guessesLeftCounter);
+    
+        document.onkeyup = function recordGuesses(e, winsCounter) {
+            var userInput = (e.key).toLowerCase();
+            $(".guessesSoFar").append(" " + userInput + " ");
+            if(randomLetter == userInput){
+                e.preventDefault();
+                winningStuff();
+            }
+            else if (randomLetter!== userInput) {
+                e.preventDefault();
+                lossesStuff();
+                
+                }
+    
+            } 
+        };  
+
+    getAlphabet();
+
+    function winningStuff(){
+        winsCounter++;
+        $(".wins").empty();
+        $(".wins").append(winsCounter);
+        getAlphabet();
+    }
+
+    function lossesStuff(){
+        guessesLeftCounter--;
+        $(".guessesLeft").append(" " + guessesLeftCounter);
+        if (guessesLeftCounter === 0) {
+            lossesCounter++;
+            $(".losses").empty();
+            $(".losses").append(" " + lossesCounter);
+            getAlphabet();
+        } else {
+            console.log("else statement excuted");
+            $(".guessesLeft").empty();
+            $(".guessesLeft").append(" " +guessesLeftCounter);
+
+        }
+    }
+    
+
+
+});
+
+
+
+
+
